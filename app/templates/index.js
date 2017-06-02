df293810d6e5ea6d83e47f2c5a85112b72b86b9b
Josiah Campbell <jbcampbe@iupui.edu>
2017-06-02 00:10:18 -0400

    hope

--- app/templates/index.js
+++
@@ -1,1 +0,0 @@
-export { default } from 'ember-engine-admin/templates/index';
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 /*jshint node:true*/
 const EngineAddon = require('ember-engines/lib/engine-addon');
+
 module.exports = EngineAddon.extend({
   name: 'ember-engine-admin',
   lazyLoading: false
