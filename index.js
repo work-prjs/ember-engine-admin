5ca8e6ac540b9ecd749980489e3c657435b6d18c
Josiah Campbell <jbcampbe@iupui.edu>
2017-06-16 15:34:54 -0400

    added ember-cli-sass and template-linter

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-/*jshint node:true*/
+/* eslint-env node */
 const EngineAddon = require('ember-engines/lib/engine-addon');
 
 module.exports = EngineAddon.extend({
-  name: 'ember-engine-admin',
-  lazyLoading: false
+    name: 'ember-engine-admin',
+    lazyLoading: false,
+    included: function() {
+        this._super.included(...arguments);
+    }
 });
